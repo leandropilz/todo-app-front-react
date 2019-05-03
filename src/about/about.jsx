88a743c70e59b68011d69aa3e3140100d60aae46
Leandro Pilz <leandro.pilzz@gmail.com>
2019-05-03 00:02:30 -0300

    Todo(Frontend) exercicio 64 concuido.

---
+++ src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Immprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
