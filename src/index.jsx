22e180b83122974e636948cd03b7e60211c731d5
Leandro Pilz <leandro.pilzz@gmail.com>
2019-05-03 21:49:50 -0300

    Adicionado React-redux e Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './main/app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const reduces = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
+ReactDOM.render(
+    <Provider store={createStore(reduces)} >
+        <App />
+    </Provider>
+    , document.getElementById('app')
+)
